de74edff7098b613f694bdd74ef3c8db13cd662b
pombnk <shardex11@gmail.com>
2022-11-24 21:56:53 +0300

    first commit

--- main.go
+++ main.go
@@ -11,7 +11,6 @@ var words string
 
 func main() {
 	reader := bufio.NewReader(os.Stdin)
-	fmt.Print("Enter text: ")
 	text, _ := reader.ReadString('\n')
 	s := strings.Fields(text)
 	fmt.Println(len(s))
