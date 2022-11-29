c85006b2c9059407dda7e9c61b4d17a7e0999586
pombnk <shardex11@gmail.com>
2022-11-29 23:12:55 +0300

    tests passed

--- main.go
+++ main.go
@@ -10,9 +10,6 @@ var counter int = 0
 
 func main() {
 	argsWithoutProg := os.Args[1:]
-	s := strings.Split(argsWithoutProg[0], " ")
-	for range s {
-		counter += 1
-	}
-	fmt.Println(counter)
+	s := strings.Fields(string(argsWithoutProg[0]))
+	fmt.Println(len(s))
 }
